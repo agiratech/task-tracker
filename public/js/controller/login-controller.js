41b87a8972a358ebd299430e735cb5663a79a895
Nagarani <nagarani@agiratech.com>
2017-04-25 18:07:48 +0530

    remove the the unwanted code

--- public/js/controller/login-controller.js
+++ public/js/controller/login-controller.js
@@ -3,21 +3,21 @@
   angular.module('TaskTrackerApp').controller('LoginController',LoginController)
   LoginController.$inject = ['$scope', '$http', '$auth', '$location', '$resource'];
   function LoginController($scope, $http,$auth, $location, $resource){
-    console.log(111)
       $scope.handleLoginBtnClick = function() {
         $auth.submitLogin($scope.loginForm)
     };
     $scope.$on('auth:login-error', function(ev, reason) {
       $scope.error = reason.errors[0];
-      console.log(reason)
-      console.log(ev)
     });
 
     $scope.$on('auth:login-success', function(ev, user) {
       $scope.user = user;
-      console.log("login-success")
       $location.path('/sample');
     });
+
+     $scope.newProject = function(){
+      $location.path('/newproject')
+    }
 
     $scope.forgotPassword = function(){
       $location.path('/forgotpassword')
@@ -27,13 +27,10 @@
     };
     $scope.$on('auth:password-reset-request-error', function(ev, reason) {
       $scope.error = reason.errors[0];
-      console.log(reason)
-      console.log(ev)
     });
 
     $scope.$on('auth:password-reset-request-success', function(ev, user) {
       $scope.user = user;
-      console.log("forgotpassword-success")
       $location.path('/sample');
     });
   }
--- public/js/controller/logout-controller.js
+++ public/js/controller/logout-controller.js
@@ -3,21 +3,19 @@
   angular.module('TaskTrackerApp').controller('LogoutController',LogoutController)
   LogoutController.$inject = ['$scope', '$http', '$auth', '$location', '$resource'];
   function LogoutController($scope, $http,$auth, $location, $resource){
-    console.log(111)
       $scope.handleSignOutBtnClick = function() {
         $auth.signOut()
       };
+    $scope.newProject = function(){
+      $location.path('/newproject')
+    }
 
       $scope.$on('auth:logout-error', function(ev, reason) {
         $scope.error = reason.errors[0];
-        console.log(reason)
-        console.log(ev)
-        console.log("hi")
       });
 
       $scope.$on('auth:logout-success', function(ev, user) {
-        // $scope.user = user;
-        console.log("logout-success")
+        $scope.user = user;
         $location.path('/login');
       });
       

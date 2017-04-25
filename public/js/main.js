e0675f93a0fd46c91fe12a20379d2d0060085063
Nagarani <nagarani@agiratech.com>
2017-04-25 18:04:55 +0530

    added controller and css in index.html

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@
 
    angular
     .module('TaskTrackerApp', ['ngRoute','ngResource', 'ng-token-auth'])
-    .config(config);
-    // .run(run);
+    .config(config)
+    .run(run);
 
     config.$inject = ['$routeProvider', '$locationProvider'];
      function config($routeProvider, $locationProvider) {
@@ -17,17 +17,24 @@
         controller: 'LogoutController',
         templateUrl: 'templates/dashboard/main.html'
        })
+       .when('/home',{
+        controller: 'LogoutController',
+        templateUrl: 'templates/dashboard/default_dashboard.html'
+       })
        .when('/forgotpassword',{
         controller: 'LoginController',
         templateUrl: 'templates/login/forgetpassword.html'
        })
+       .when('/newproject',{
+        controller: 'ProjectController',
+        templateUrl: 'templates/projects/new_project.html'
+       })
        .otherwise({ redirectTo: '/' });
       }
 
-    // run.$inject = ['$rootScope', '$location'];
-    // function run($rootScope, $location) {
-    // console.log("test")
-    //   $location.path('/login')
-    // }
+    run.$inject = ['$rootScope', '$location'];
+    function run($rootScope, $location) {
+    $rootScope.location = $location
+    }
 
 })();
